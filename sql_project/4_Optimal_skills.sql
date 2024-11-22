5bce048527e8951a791f215c5d4a11bad5efdcd7
King Nghi <trankiennghi2003@gmail.com>
2024-11-22 11:25:39 +0700

    Upload query 4

--- sql_project/4_Optimal_skills.sql
+++ sql_project/4_Optimal_skills.sql
@@ -6,23 +6,23 @@ for career development in data analysis
 */
 WITH skill_demand AS(
     SELECT 
-        skills,
-        COUNT(skills_job_dim.skill_id) AS demand_count
+        COUNT(skills_job_dim.skill_id) AS demand_count,
+        skills_dim.skill_id,
+        skills_dim.skills
     FROM job_postings_fact
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE 
         job_title_short = 'Data Analyst'
-        salary_year_avg IS NOT NULL 
+        AND salary_year_avg IS NOT NULL 
         AND job_work_from_home = TRUE
     GROUP BY 
-        skill_id
-)
-
-WITH average_salary AS(
+        skills_dim.skill_id
+),
+    average_salary AS(
     SELECT
-        skills,
-        ROUND(AVG(salary_year_avg),0) AS avg_salary
+        ROUND(AVG(salary_year_avg),0) AS avg_salary,
+        skills_job_dim.skill_id
     FROM job_postings_fact 
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
@@ -31,5 +31,19 @@ WITH average_salary AS(
         AND job_title_short = 'Data Analyst'
         AND job_work_from_home = TRUE
     GROUP BY 
-        skill_id
+        skills_job_dim.skill_id
 )
+
+SELECT 
+    skill_demand.skill_id,
+    skill_demand.skills,
+    demand_count,
+    avg_salary
+FROM skill_demand
+JOIN average_salary
+ON skill_demand.skill_id = average_salary.skill_id
+WHERE demand_count > 10
+ORDER BY 
+    avg_salary DESC, 
+    demand_count DESC
+LIMIT 25
